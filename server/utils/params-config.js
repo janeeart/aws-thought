96aa424ebd50a183aba447a5a08b2d14cf40bfc5
Janée Artis <janeeartis@gmail.com>
2021-08-22 14:21:17 -0600

    image upload successful

---
+++ server/utils/params-config.js
@@ -0,0 +1,16 @@
+const { v4: uuidv4 } = require('uuid');
+
+const params = fileName => {
+    const myFile = fileName.originalname.split('.');
+    const fileType = myFile[myFile.length - 1];
+  
+    const imageParams = {
+      Bucket: 'user-images-51ea3ab5-b4c8-40e2-8c92-441363eef014',
+      Key: `${uuidv4()}.${fileType}`,
+      Body: fileName.buffer
+    };
+  
+    return imageParams;
+  };
+
+  module.exports = params;
